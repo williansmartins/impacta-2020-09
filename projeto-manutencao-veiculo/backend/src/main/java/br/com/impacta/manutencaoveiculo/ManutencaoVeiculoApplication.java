d06d5dd114b4ec286569311f3f7727d2238d1227
Willians Martins <professorwillians@gmail.com>
2020-09-24 20:17:24 -0300

    migracao para nuvem

--- projeto-manutencao-veiculo/backend/src/main/java/br/com/impacta/manutencaoveiculo/ManutencaoVeiculoApplication.java
+++ projeto-manutencao-veiculo/backend/src/main/java/br/com/impacta/manutencaoveiculo/ManutencaoVeiculoApplication.java
@@ -2,9 +2,10 @@ package br.com.impacta.manutencaoveiculo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ManutencaoVeiculoApplication {
+public class ManutencaoVeiculoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManutencaoVeiculoApplication.class, args);
