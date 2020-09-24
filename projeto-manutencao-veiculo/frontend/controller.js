c8efa1a16467620550720ce7f2c45b68b1635c46
Willians Martins <professorwillians@gmail.com>
2020-09-23 22:48:02 -0300

    add bootstrap

--- projeto-manutencao-veiculo/frontend/controller.js
+++ projeto-manutencao-veiculo/frontend/controller.js
@@ -1,25 +1,88 @@
 var app = angular.module('myApp', []);
-
 app.controller('myCtrl', function($scope, $http) {
-  $scope.nome = "Willians";
-  $scope.dia = "11/12/2020";
-  $scope.valor = "1.49";
-  $scope.manutencoes = new Object();
+  
+  $scope.preencher = function(){
+    $scope.dia = "2020-09-24";
+    $scope.nome = "Troca de óleo";
+    $scope.valor = "36.00";
+  }
 
+  $scope.insert = function(){
+    $http.post("http://localhost:8080/manutencoes", {
+      'valor':$scope.valor,
+      'nome':$scope.nome,
+      'dia':$scope.dia
+    })
+    .then(function(response){
+      $scope.manutencoes = response.data;
+      $scope.getTable();
+    });
+  };    
+
+  $scope.clear = function(){
+    $scope.id = "";
+    $scope.dia = "";
+    $scope.nome = "";
+    $scope.valor = "";
+  }
+
+  $scope.getTable = function(){
+    $http.get("http://localhost:8080/manutencoes")
+    .then(function(response){
+      $scope.manutencoes = response.data;
+    });
+  }
+
+  $scope.getOne = function(id){
+    $http.get("http://localhost:8080/manutencoes/" + id)  
+    .then(function(response) {
+      $scope.id=response.data.id;
+      $scope.dia=response.data.dia;
+      $scope.nome=response.data.nome;
+      $scope.valor=response.data.valor;
+    });
+  }
+  
   $scope.getAll = function(){
     $http.get("http://localhost:8080/manutencoes")
-    .then(function(resposta) {
-      $scope.manutencoes = resposta.data; //array
-    });
+    .then(function(response) {
+        console.info(response);
+    });    
   }
 
   $scope.delete = function(id){
     $http.delete("http://localhost:8080/manutencoes/" + id)
-    .then(function(resposta) {
-      $scope.getAll();
+    .then(function(response){
+    $scope.getTable();    
     });
   }
 
-  $scope.getAll();
+  $scope.put = function(id){
+    $http.put("http://localhost:8080/manutencoes/" + id,{
+    'valor':$scope.valor,
+    'nome':$scope.nome,
+    'dia':$scope.dia
+    })
+    .then(function(response){
+      $scope.manutencoes = response.data;
+      $scope.getTable();
+    });   
+  }
 
+  $scope.save = function(){
+  if ($scope.dia == "") {
+    console.info("Campo em Branco!")
+  } else {
+    if ($scope.id) {
+      $scope.put($scope.id);
+    } else {
+      $scope.insert();
+    }    
+    console.info("Save Success!")
+    $scope.clear();
+    $scope.getTable();
+  }
+}  
+
+  $scope.getTable();
 });
